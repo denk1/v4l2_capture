ba5c927a5e755843c190a645a171800e34045d38
Your Name <tu214@yandex.ru>
2020-10-02 12:50:51 +0300

    base working

---
+++ capture.h
@@ -0,0 +1,35 @@
+ #ifndef CAPTURE_H
+ #define CAPTURE_H
+ #define CLEAR(x) memset(&(x), 0, sizeof(x))
+ #pragma message("test")
+    enum io_method {
+	    IO_METHOD_READ,
+	    IO_METHOD_MMAP,
+	    IO_METHOD_USERPTR,
+    };
+
+    struct buffer {
+	    void   *start;
+	    size_t  length;
+    };
+
+    static const char            *dev_name =  "/dev/video0";
+    static enum io_method   io = IO_METHOD_MMAP;
+    static int              fd = -1;
+    static struct buffer          *buffers;
+    static unsigned int     n_buffers;
+    static int              out_buf;
+    static int              force_format;
+    static int              frame_count = 70;
+    inline unsigned char my_frame[614400];
+
+
+void open_device();
+void init_device();
+void start_capturing();
+int read_frame(void);
+void main_step();
+void stop_capturing();
+void uninit_device();
+void close_device();
+#endif
---
+++ video_capture.cpp
@@ -0,0 +1,92 @@
+#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
+#include "capture.h"
+
+using namespace cv;
+using namespace std;
+
+void init_video_capture()
+{
+    open_device();
+    init_device();
+    start_capturing();
+}
+
+void clear_video_capture()
+{
+    stop_capturing();
+    uninit_device();
+    close_device();
+}
+
+void set_frame_buffer() {
+
+}
+
+Mat get_frame_from_file()
+{
+    FILE *f = fopen("frame.bin", "rb");
+    fread(my_frame, 614400, 1, f);
+    Mat fr_test(480, 640, CV_8UC2, (void *)&my_frame);
+    Mat bgr_img(480, 640, CV_8UC3);
+    cvtColor(fr_test, bgr_img, COLOR_YUV2BGR_YUYV);
+    fclose(f);
+    return bgr_img;
+}
+
+
+Mat get_frame() {
+    Mat fr_test(480, 640, CV_8UC2, (void *)&my_frame);
+    Mat bgr_img(480, 640, CV_8UC3);
+    cvtColor(fr_test, bgr_img, COLOR_YUV2BGR_YUYV);
+    return bgr_img;
+}
+
+int main(int, char **)
+{
+    Mat frame(480, 640, CV_8UC3), frame1(512, 512, 0);
+
+    //--- INITIALIZE VIDEOCAPTURE
+    //VideoCapture cap;
+    // open the default camera using default API
+    // cap.open(0);
+    // OR advance usage: select any API backend
+    int deviceID = 0;        // 0 = open default camera
+    int apiID = cv::CAP_ANY; // 0 = autodetect default API
+
+    // open selected camera using selected API
+    //cap.open(deviceID, apiID);
+    // check if we succeeded
+    //if (!cap.isOpened()) {
+    //    cerr << "ERROR! Unable to open camera\n";
+    //    return -1;
+    //}
+    //--- GRAB AND WRITE LOOP
+    cout << "Start grabbing" << endl
+         << "Press any key to terminate" << endl;
+    init_video_capture();
+    for (;;)
+    {
+        // wait for a new frame from camera and store it into 'frame'
+        // cap.read(frame);
+        // check if we succeeded
+        // if (frame.empty()) {
+        //    cerr << "ERROR! blank frame grabbed\n";
+        //    break;
+        // }
+        // show live and wait for a key with timeout long enough to show images
+        main_step();
+        //frame = get_frame_from_file();
+        frame = get_frame();
+        imshow("Live", frame);
+        if (waitKey(1) >= 0)
+            break;
+    }
+    clear_video_capture();
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
